dbe49dd6eae454ad813a0a6a30b8b59c2cf20532
Dong Yeop Lee <dongy7@gmail.com>
2015-11-29 15:48:29 -0500

    add placeholder for settings

--- components/Setting.js
+++ components/Setting.js
@@ -1,10 +1,68 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import SelectField from 'material-ui/lib/select-field'
+
+const arbitraryArrayMenuItems = [
+  {id:1, name:'Option 1'},
+  {id:2, name:'Option 2'},
+  {id:3, name:'Option 3'},
+  {id:4, name:'Option 4'},
+  {id:5, name:'Option 5'},
+]
+
+const styles = {
+  textfield: {
+    marginLeft: 10,
+    marginTop: 10,
+  }
+}
 
 class Setting extends Component {
+  constructor() {
+    super()
+    this.state = {
+      selectValue1: 1,
+      selectValue2: 1,
+      selectValue3: 1,
+    }
+  }
+
+
+  _handleSelectValueChange(name, e) {
+    const change = {}
+    change[name] = e.target.value
+    this.setState(change);
+  }
+
   render() {
     return (
-      <div className="page">
-        <h1>Settings</h1>
+      <div className="settings">
+        <SelectField
+          floatingLabelText="Setting 1"
+          style={styles.textfield}
+          value={this.state.selectValue1}
+          valueMember="id"
+          displayMember="name"
+          onChange={this._handleSelectValueChange.bind(this, 'selectValue1')}
+          menuItems={arbitraryArrayMenuItems} />
+        <br/>
+        <SelectField
+          floatingLabelText="Setting 2"
+          style={styles.textfield}
+          value={this.state.selectValue2}
+          valueMember="id"
+          displayMember="name"
+          onChange={this._handleSelectValueChange.bind(this, 'selectValue2')}
+          menuItems={arbitraryArrayMenuItems} />
+        <br/>
+        <SelectField
+          floatingLabelText="Setting 3"
+          style={styles.textfield}
+          value={this.state.selectValue3}
+          valueMember="id"
+          displayMember="name"
+          onChange={this._handleSelectValueChange.bind(this, 'selectValue3')}
+          menuItems={arbitraryArrayMenuItems} />
+        <br/>
       </div>
     )
   }
