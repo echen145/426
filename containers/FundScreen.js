0cc8fccd4798c20895cb81c576bfd0bd81173150
Dong Yeop Lee <dongy7@gmail.com>
2015-12-06 23:21:20 -0500

    use screen width for fund map view

--- containers/FundScreen.js
+++ containers/FundScreen.js
@@ -26,16 +26,13 @@ class FundScreen extends Component {
         <Tab label="Map" >
           <div className="container-fluid">
           <div className="row">
-            <div className="col-sm-2">
-            </div>
-            <div className="col-sm-8">
+            <div className="col-sm-12">
               <FundMap 
                 map={map} 
                 fund={fund}
                 />   
             </div>
-            <div className="col-sm-2">
-            </div>
+
           </div>
           </div>
         </Tab>
